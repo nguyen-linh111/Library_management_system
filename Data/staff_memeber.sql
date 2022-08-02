974a8abb4c8f972d7c58176c35b931c6b0ee849d
nguyen-linh111 <71222517+nguyen-linh111@users.noreply.github.com>
2022-08-02 17:00:23 +0700

    Add files via upload

---
+++ Data/staff_memeber.sql
@@ -0,0 +1,109 @@
+CREATE TABLE MEMBER
+(
+	MEMBER_ID VARCHAR(10) PRIMARY KEY,
+	FIRST_NAME VARCHAR(50),
+	LAST_NAME VARCHAR(50),
+	M_ADDRESS VARCHAR(255),
+	BIRTHDAY DATETIME,
+	ID VARCHAR(20),
+	EMAIL VARCHAR(100),
+	PASSWORD VARCHAR(100),
+	PHONE_NUM VARCHAR(50)
+)
+
+INSERT INTO MEMBER VALUES('FG156045', 'Norman ', 'Osuna', '345 Hillside Drive', '1975-12-01 00:00:00',  'H8196987412', 'foster_moe3@hotmail.com', 'XZf7Rs9T', '020-46545496')
+INSERT INTO MEMBER VALUES('CA163551', 'John ', 'Martin', '3561 Duke Lane', '1955-04-22 00:00:00',  'E1408805205', 'naomie_langwor@gmail.com', 'VKtQg43k', '020-27220889')
+INSERT INTO MEMBER VALUES('CH741977', 'Clarence ', 'Davis', '144 Kelly Drive', '1987-03-08 00:00:00',  'C6820666287', 'kaci.kunz7@gmail.com', 'BwWQNhyN', '020-45081653')
+INSERT INTO MEMBER VALUES('CA240857', 'Paula ', 'Gillum', '74 Spring Avenue', '1968-02-07 00:00:00',  'F3541639482', 'rowan.halvors@hotmail.com', 'rCCBhxct', '020-38658040')
+INSERT INTO MEMBER VALUES('BH540477', 'Tanya ', 'Perry', '424 Jim Rosa Lane', '1974-12-28 00:00:00',  'F5947932020', 'arno1974@yahoo.com', '2PdAMfBv', '020-74032880')
+INSERT INTO MEMBER VALUES('GA151753', 'Paul', 'Parrish', '2185 Alpaca Way', '1991-03-13 00:00:00',  'I7359774012', 'kaitlyn1991@yahoo.com', '5L6hbTKt', '020-16997060')
+INSERT INTO MEMBER VALUES('HB965115', 'Margie ', 'Graves', '1100 Mahlon Street', '1982-08-11 00:00:00',  'C9802580964', 'sophia1982@hotmail.com', '3hppaD4m', '020-54228091')
+INSERT INTO MEMBER VALUES('AA477915', 'Heather ', 'Herzog', '3314 Masonic Hill Road', '1956-05-30 00:00:00',  'E8348015867', 'kristian1956@hotmail.com', '12dsaAce', '020-27665522')
+INSERT INTO MEMBER VALUES('FF107722', 'Charles', 'Amy', '383 Watson Street', '1993-08-02 00:00:00',  'E2480995246', 'terrence1993@hotmail.com', 'rAMN4EJ6', '020-65190049')
+INSERT INTO MEMBER VALUES('GD644041', 'Joyce ', 'Smith', '4502 Ridge Road', '1981-09-07 00:00:00',  'F4740883687', 'billie.scho@hotmail.com', '43QnKuDa', '020-23084859')
+INSERT INTO MEMBER VALUES('GF448228', 'Mikki ', 'Mikki ', '41 Hope Street', '1976-04-08 00:00:00',  'G3197805883', 'christ2002@yahoo.com', 'c5FeaLCW', '020-55167804')
+INSERT INTO MEMBER VALUES('AE456176', 'Ashley', 'Johnston', '37 Mercer Street', '1976-08-13 00:00:00',  'B7449858046', 'odea.oreill@hotmail.com', 'tLZxJYp2', '020-76442949')
+INSERT INTO MEMBER VALUES('ID935100', 'Margaret ', 'McEachern', '3786 Allison Avenue', '1962-02-10 00:00:00',  'B5992309151', 'dell.kilbac4@gmail.com', 'bCTtdyVb', '020-86805414')
+INSERT INTO MEMBER VALUES('BH607561', 'Georgia ', 'Mickel', '8 Hillcrest Circle', '1972-10-06 00:00:00',  'G6185261957', 'alberta1972@yahoo.com', 'crw3GC8c', '020-30491682')
+INSERT INTO MEMBER VALUES('HG721320', 'Claude', 'Jefferies', '2324 Smithfield Avenue', '1972-11-09 00:00:00',  'F6105286560', 'cleora1973@hotmail.com', 'khFpK7KC', '020-94939294')
+INSERT INTO MEMBER VALUES('AC666869', 'Donald ', 'Hibner', '34 South Street', '1971-07-12 00:00:00',  'C3138354990', 'candido2010@hotmail.com', '3AGRVUDX', '020-31769290')
+INSERT INTO MEMBER VALUES('FB787898', 'Lawrence ', 'Lewis', '2331 Rosebud Avenue', '1963-12-13 00:00:00',  'F4743734660', 'kianna_fram9@gmail.com', 's4g8xsdf', '020-17953317')
+INSERT INTO MEMBER VALUES('BA522480', 'Keith ', 'Butler', '242 Powder House Road', '1987-08-25 00:00:00',  'A6043290050', 'zoey_goldne5@yahoo.com', 'tJrvL9Dg', '020-64259218')
+INSERT INTO MEMBER VALUES('IF251545', 'Edna ', 'Tucker', '2446 Edington Drive', '1962-06-16 00:00:00',  'D8483463643', 'matt_wisok9@gmail.com', 'ppZm4REt', '020-36264493')
+INSERT INTO MEMBER VALUES('EA362778', 'David ', 'Shivers', '1631 Cinnamon Lane', '1990-03-19 00:00:00',  'E1455077973', 'omer2016@gmail.com', 'B2S4Agwx', '020-29634176')
+INSERT INTO MEMBER VALUES('HF488022', 'Doris ', 'Martin', '512 Hope Street', '1993-03-03 00:00:00',  'F6078838952', 'tevin1995@hotmail.com', '6AtcsjGz', '020-11579746')
+INSERT INTO MEMBER VALUES('FC163629', 'Ricky', 'Vivanco', '91 Cantebury Drive', '1997-03-16 00:00:00',  'F1429374157', 'pearl2002@hotmail.com', 'LEJ58Pym', '020-38529955')
+INSERT INTO MEMBER VALUES('HE722514', 'Larry ', 'Larry ', '279 Anthony Avenue', '1984-11-13 00:00:00',  'A6974452396', 'jennings_man@gmail.com', 'jpDyfNxv', '020-85177552')
+INSERT INTO MEMBER VALUES('GG268562', 'Karen ', 'Davis', '473 Jerome Avenue', '1973-08-26 00:00:00',  'F3499548513', 'sadie1975@gmail.com', 'cWRHYjtX', '020-83473843')
+INSERT INTO MEMBER VALUES('HB361155', 'David ', 'Rucker', '418 Jerry Dove Drive', '1985-12-03 00:00:00',  'H9623579034', 'rozella_corke@hotmail.com', 'W6nLa7C6', '020-77985796')
+INSERT INTO MEMBER VALUES('GB493876', 'Robert ', 'Long', '3941 Hart Street', '1965-04-06 00:00:00',  'G9603010599', 'daryl.star2@hotmail.com', 'bMyZ7g7j', '020-28484985')
+INSERT INTO MEMBER VALUES('HB573283', 'David ', 'Frasher', '1240 Public Works Drive', '1955-12-06 00:00:00',  'I7190178629', 'golda2003@gmail.com', 'TcmbkJQe', '020-72483128')
+INSERT INTO MEMBER VALUES('GI405583', 'Walter ', 'Brewster', ' 40 Eva Pearl Street', '1993-03-28 00:00:00',  'F6999060632', 'lamont1980@gmail.com', 'PFSUNjE3', '020-99367028')
+INSERT INTO MEMBER VALUES('CD789964', 'James ', 'Fain', '28 Owen Lane', '1955-01-29 00:00:00',  'G9292032101', 'nadia_koelp@hotmail.com', 'ysRYfpd7', '020-91453737')
+INSERT INTO MEMBER VALUES('AC833106', 'Tom ', 'Campbell', '148 Bastin Drive', '1981-12-12 00:00:00',  'G5375248984', 'nayeli2005@yahoo.com', 'QLKUVL4r', '020-82556109')
+INSERT INTO MEMBER VALUES('IC699356', 'Lena ', 'Davis', '468 College Street', '1964-02-18 00:00:00',  'C5214620955', 'declan1991@yahoo.com', '85abXCvY', '020-18323327')
+INSERT INTO MEMBER VALUES('FD482100', 'Linda ', 'Campas', '621 Gregory Lane', '1973-10-09 00:00:00',  'D6526727893', 'justyn.col7@gmail.com', 'u3zTk2Gy', '020-30955607')
+INSERT INTO MEMBER VALUES('IH424879', 'Earnestine ', 'Black', '3950 Lamberts Branch Road', '1979-04-21 00:00:00',  'F9263714638', 'lincoln1976@yahoo.com', 'aTV7dfYF', '020-57214914')
+INSERT INTO MEMBER VALUES('BD999281', 'William ', 'Lundy', '1795 Golden Street', '1989-07-26 00:00:00',  'E6126907069', 'tom.lueilwi@gmail.com', 'tRPb5NLd', '020-99013394')
+INSERT INTO MEMBER VALUES('GH667043', 'Rosa ', 'Varela', '3538 Oxford Court', '1965-10-26 00:00:00',  'F6501317260', 'parker2010@yahoo.com', 'UU7Vtscb', '020-80166468')
+INSERT INTO MEMBER VALUES('FF880823', 'James ', 'Creech', '28 West Fork Street', '1950-11-09 00:00:00',  'G1979165492', 'leonard1988@yahoo.com', '8uAE4p9S', '020-98947429')
+INSERT INTO MEMBER VALUES('GD917626', 'Ollie ', 'Jackson', '4328 Big Indian', '1964-11-21 00:00:00',  'I2354326712', 'armando2015@hotmail.com', 'wPCd6shA', '020-25185602')
+INSERT INTO MEMBER VALUES('GI396298', 'Denise ', 'Adkins', '2327 Bassel Street', '1951-07-25 00:00:00',  'D3339972317', 'matteo.corke@yahoo.com', 'BkkHgJK4', '020-33821714')
+INSERT INTO MEMBER VALUES('DA800392', 'Billy ', 'Roberts', '16 Late Avenue', '1974-01-15 00:00:00',  'F6700238464', 'roma.doyl1@hotmail.com', 'aPJ7vGwQ', '020-35222908')
+INSERT INTO MEMBER VALUES('AG120996', 'David ', 'Parker', '2216 Green Acres Road', '1956-10-29 00:00:00',  'E4726765217', 'arnoldo.macejkov@gmail.com', 'BkkHgJK4', '020-10096132')
+INSERT INTO MEMBER VALUES('DE883346', 'Jennifer ', 'Murphy', '9 Norman Street', '1972-10-31 00:00:00',  'B1773364804', 'delia_crook9@hotmail.com', 'Y3gnjant', '020-30953087')
+INSERT INTO MEMBER VALUES('II742800', 'James ', 'Lowe', '89 Grim Avenue', '1992-09-21 00:00:00',  'G3569891136', 'orin1993@gmail.com', '9zYxTtse', '020-29770452')
+INSERT INTO MEMBER VALUES('BE496301', 'Tony ', 'Patel', '333 Jennifer Lane', '1978-10-14 00:00:00',  'H4303550701', 'pearl_ledne10@hotmail.com', 'H8G3Nn76', '020-84553654')
+INSERT INTO MEMBER VALUES('HB855934', 'Virginia ', 'Law', '242 Heritage Road', '1958-08-23 00:00:00',  'B8155132348', 'kade1970@gmail.com', 'QZRCWr34', '020-20348641')
+INSERT INTO MEMBER VALUES('CC219637', 'Jesse ', 'Clothier', '2487 Wildwood Street', '1979-11-29 00:00:00',  'B6565346386', 'estefania.cri@gmail.com', 'WQqADcVS', '020-29518919')
+INSERT INTO MEMBER VALUES('HG282354', 'Jason ', 'Hasson', '1963 Masonic Hill Road', '1981-08-18 00:00:00',  'E4288212924', 'meagan.col9@yahoo.com', 'vJZu6bnY', '020-34518539')
+INSERT INTO MEMBER VALUES('DB228004', 'George ', 'Shaw', '99 Sycamore Road', '1973-01-02 00:00:00',  'I2573554450', 'milo.wolf1999@yahoo.com', '24mQfdYb', '020-79074790')
+INSERT INTO MEMBER VALUES('EH301014', 'Patti ', 'Fajardo', '447 State Street', '1967-07-20 00:00:00',  'D3087465808', 'isaac.zemla3@hotmail.com', 'FtUM3ezb', '020-70021528')
+INSERT INTO MEMBER VALUES('DG216156', 'Paula ', 'Etchison', '149 Fleming Way', '1957-11-06 00:00:00',  'H9124134696', 'macy2005@yahoo.com', '3h9nFSYt', '020-88326888')
+INSERT INTO MEMBER VALUES('BE274623', 'Kristen ', 'Holt', '4914 Oak Drive', '1984-05-11 00:00:00',  'E5712751250', 'erica.ratk4@gmail.com', 'J5GPKd7c', '020-24258677')
+
+
+
+
+CREATE TABLE STAFF
+(
+	STAFF_ID VARCHAR(10) PRIMARY KEY,
+	FULLNAME VARCHAR(100),
+	TITLE VARCHAR(50),
+	PHONE_NUM VARCHAR(50),
+	M_ADDRESS VARCHAR(255)
+)
+
+INSERT INTO STAFF VALUES('4806A', 'Aretha Franklyn', 'Librarian', '020-40781209', '2235  Hayhurst Lane')
+INSERT INTO STAFF VALUES('5072B', 'Marcia Daubney', 'Guard', '020-31869796', '35  Shannon Way')
+INSERT INTO STAFF VALUES('9540H', 'Zak Bisset', 'Cleaning Staff', '020-79036367', '60  South Crescent')
+INSERT INTO STAFF VALUES('4749E', 'Carmen Leonardson', 'President', '020-24121786', '2457  Burnside Avenue')
+INSERT INTO STAFF VALUES('8223B', 'Leonard Hatheway', 'IT Staff', '020-44600087', '1096  Rosewood Lane')
+INSERT INTO STAFF VALUES('9339C', 'Cherry Byrd', 'Cleaning Staff', '020-94359741', '44  Station Rd')
+INSERT INTO STAFF VALUES('2128I', 'Haven Rollins', 'IT Staff', '020-46025629', '74  Ross Road')
+INSERT INTO STAFF VALUES('3908B', 'Gary Page', 'Guard', '020-46035793', '4518  Bicetown Road')
+INSERT INTO STAFF VALUES('5889G', 'Alfreda Elder', 'Staff', '020-40042333', '92  Greyfriars Road')
+INSERT INTO STAFF VALUES('8181G', 'Wright Carlisle', 'Vice President', '020-38672969', '226  Indiana Avenue')
+INSERT INTO STAFF VALUES('6294H', 'Jessa Mutton', 'IT Staff', '020-19709319', '4313  Poplar Lane')
+INSERT INTO STAFF VALUES('2447F', 'Nannie Gary', 'Staff', '020-48099589', '41  Redcliffe Way')
+INSERT INTO STAFF VALUES('3776I', 'Dean Hutson', 'Guard', '020-78829634', '1506  Hiney Road')
+INSERT INTO STAFF VALUES('4199A', 'Seth Triggs', 'Librarian', '020-39650053', '70  Telford Street')
+INSERT INTO STAFF VALUES('5334F', 'Raphael Sanford', 'Cleaning Staff', '020-10984107', '4274  Simons Hollow Road')
+INSERT INTO STAFF VALUES('7018D', 'Daniella Mynatt', 'Cleaning Staff', '020-83027185', '629  Karen Lane')
+INSERT INTO STAFF VALUES('8909H', 'Zac Mallory', 'Guard', '020-69970854', '2863  Zimmerman Lane')
+INSERT INTO STAFF VALUES('5452E', 'Milburn Wilson', 'Staff', '020-19740902', '84  Roman Rd')
+INSERT INTO STAFF VALUES('5928D', 'Cara Darwin', 'Librarian', '020-13670968', '124  Philadelphia Avenue')
+INSERT INTO STAFF VALUES('6522E', 'Ian Graves', 'Guard', '020-94324145', '4552  Passaic Street')
+INSERT INTO STAFF VALUES('2087F', 'Zandra Lincoln', 'Cleaning Staff', '020-57587810', '3075  Buck Drive')
+INSERT INTO STAFF VALUES('1006F', 'Mary Jo Dale', 'Guard', '020-78510214', '95  Abbey Row')
+INSERT INTO STAFF VALUES('9724A', 'Charity Carman', 'Staff', '020-37380056', '3885  Emma Street')
+INSERT INTO STAFF VALUES('2413H', 'Diann Seward', 'IT Staff', '020-73511375', '108  Caxton Place')
+INSERT INTO STAFF VALUES('5765E', 'Ferdy Hamm', 'Vice President', '020-51851693', '727  Ryder Avenue')
+INSERT INTO STAFF VALUES('2202E', 'Nolan Hunnisett', 'Cleaning Staff', '020-34163680', '1397  Fittro Street')
+INSERT INTO STAFF VALUES('9473G', 'Suzy Andrews', 'IT Staff', '020-38282648', '70  Bootham Crescent')
+INSERT INTO STAFF VALUES('1543C', 'Carmella Wright', 'Librarian', '020-15686355', '3991  Cottonwood Lane')
+INSERT INTO STAFF VALUES('6259B', 'Daria Horsfall', 'Librarian', '020-82881058', '185  Baker Avenue')
+INSERT INTO STAFF VALUES('8083C', 'Queen Harrelson', 'Cleaning Staff', '020-82637565', '2733  Graystone Lakes')
+
+
+
